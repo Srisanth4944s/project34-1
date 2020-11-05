60937f3be5a7e7ece03794fad6f68b3f7c96ef56
Srisanth4944s <70645902+Srisanth4944s@users.noreply.github.com>
2020-11-05 20:57:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var newball,position,database;
+var dog , happydog , database , foodS , foodstock
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    newball = createSprite(250,250,10,10);
+    newball.shapeColor = "red";
+    var newballposition = database.ref("ball/position")
+    newballposition.on("value",readposition,showerror)
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        'x':position.x+x,
+        'y':position.y+y,
+    })
+}
+
+function readposition(data){
+    position = data.val()
+    newball.x = position.x
+    newball.y = position.y
+}
+
+function showerror(){
+    console.log("error")
+}
